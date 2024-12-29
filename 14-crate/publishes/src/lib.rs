c3d88c47d5d674da4df59fe6c11fdb6ffa18259c
mirz <mirza.romi@doku.com>
2024-12-29 13:53:00 +0700

    add example crate

---
+++ 14-crate/publishes/src/lib.rs
@@ -0,0 +1,12 @@
+/// Adds one to the number given.
+/// 
+/// # Example
+/// ```
+/// let arg = 5;
+/// let answer = publishes::add_one(arg);
+/// 
+/// assert_eq!(6, answer);
+/// ```
+pub fn add_one(input: i32) -> i32 {
+    input + 1
+}
